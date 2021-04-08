58304cf8f41421d7b0bc3228bcddf8b226373f7e
Rob <rob@nc-ass-vip.sdv.fr>
2021-04-08 14:25:50 +0200

    Animate offline screen ('cause you know eye candy and shit)

--- app/src/main/java/org/robnetwork/led/ui/MainActivity.kt
+++ app/src/main/java/org/robnetwork/led/ui/MainActivity.kt
@@ -1,6 +1,10 @@
 package org.robnetwork.led.ui
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.view.View
+import android.view.ViewAnimationUtils
+import androidx.annotation.StringRes
 import org.robnetwork.led.R
 import org.robnetwork.led.databinding.ActivityMainBinding
 import org.robnetwork.led.model.MainData
@@ -15,22 +19,13 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainData, MainViewModel>(
     }
 
     override fun updateUI(binding: ActivityMainBinding, data: MainData) {
-        when(data.connexionStatus) {
-            MainData.ConnexionStatus.UNREACHABLE -> {
-                binding.downView.visibility = View.VISIBLE
-                binding.downText.visibility = View.VISIBLE
-                binding.downText.setText(R.string.server_down)
-            }
-            MainData.ConnexionStatus.OFF -> {
-                binding.downView.visibility = View.VISIBLE
-                binding.downText.visibility = View.VISIBLE
-                binding.downText.setText(R.string.equalizer_offline)
-            }
-            MainData.ConnexionStatus.ON -> {
-                binding.downView.visibility = View.GONE
-                binding.downText.visibility = View.GONE
-                binding.downText.setText(R.string.server_down)
-            }
+        when (data.connexionStatus) {
+            MainData.ConnexionStatus.UNREACHABLE ->
+                binding.takeIf { !it.downViewVisible() }?.revealDownView(R.string.server_down)
+            MainData.ConnexionStatus.OFF ->
+                binding.takeIf { !it.downViewVisible() }?.revealDownView(R.string.equalizer_offline)
+            MainData.ConnexionStatus.ON ->
+                binding.takeIf { it.downViewVisible() }?.hideDownView()
         }
         data.config?.let { config ->
             binding.toolbar.toolbarPower.setOnClickListener {
@@ -39,4 +34,46 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainData, MainViewModel>(
             binding.toolbar.toolbarSoundToggle.setOnClickListener { viewModel.toggleSound() }
         }
     }
+
+    private fun ActivityMainBinding.revealDownView(@StringRes textId: Int) {
+        val powerHalfWidth = toolbar.toolbarPower.width / 2
+        val cx = toolbar.toolbarPower.x.toInt() + powerHalfWidth
+        val cy = toolbar.toolbarPower.y.toInt() + powerHalfWidth
+        val finalRadius = (downView.height + powerHalfWidth).toFloat()
+        ViewAnimationUtils
+            .createCircularReveal(downView, cx, cy, powerHalfWidth.toFloat(), finalRadius)
+            .setDuration(1000)
+            .also {
+                downView.visibility = View.VISIBLE
+                it.addListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator?) {
+                        downText.visibility = View.VISIBLE
+                        downText.setText(textId)
+                    }
+                })
+            }
+            .start()
+    }
+
+    private fun ActivityMainBinding.hideDownView() {
+        val powerHalfWidth = toolbar.toolbarPower.width / 2
+        val cx = toolbar.toolbarPower.x.toInt() + powerHalfWidth
+        val cy = toolbar.toolbarPower.y.toInt() + powerHalfWidth
+        val radius = (downView.height + powerHalfWidth).toFloat()
+        ViewAnimationUtils
+            .createCircularReveal(downView, cx, cy, radius, powerHalfWidth.toFloat())
+            .setDuration(1000)
+            .also {
+                downText.visibility = View.GONE
+                it.addListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator?) {
+                        super.onAnimationEnd(animation)
+                        downView.visibility = View.GONE
+                    }
+                })
+            }
+            .start()
+    }
+
+    private fun ActivityMainBinding.downViewVisible(): Boolean = downView.visibility == View.VISIBLE
 }
